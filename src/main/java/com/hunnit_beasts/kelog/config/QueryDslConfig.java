0ce93bb9216f2cb11fb9630f766fefddafd0b833
yu-teacher <yusm1231@gmail.com>
2024-06-08 06:08:49 +0900

    refactor : import문 정리

--- src/main/java/com/hunnit_beasts/kelog/config/QueryDslConfig.java
+++ src/main/java/com/hunnit_beasts/kelog/config/QueryDslConfig.java
@@ -3,7 +3,6 @@ package com.hunnit_beasts.kelog.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 

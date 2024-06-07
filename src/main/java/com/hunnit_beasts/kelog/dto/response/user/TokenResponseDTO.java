2c63143fee307ccc9c7d15e3e7c1405a595bdbfa
yu-teacher <yusm1231@gmail.com>
2024-06-08 05:37:59 +0900

    feat : 로그인 기능 완성 및 스프링 시큐리티 설정 완성
    아직 리펙토링이 필요할 것 같음

--- src/main/java/com/hunnit_beasts/kelog/dto/response/user/TokenResponseDTO.java
+++ src/main/java/com/hunnit_beasts/kelog/dto/response/user/TokenResponseDTO.java
@@ -1,8 +1,12 @@
 package com.hunnit_beasts.kelog.dto.response.user;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TokenResponseDTO {
-    private final String token;
+    private String token;
 }
--- src/main/java/com/hunnit_beasts/kelog/serviceimpl/AuthServiceImpl.java
+++ src/main/java/com/hunnit_beasts/kelog/serviceimpl/AuthServiceImpl.java
@@ -1,24 +1,30 @@
 package com.hunnit_beasts.kelog.serviceimpl;
 
+import com.hunnit_beasts.kelog.dto.request.user.UserCreateRequestDTO;
 import com.hunnit_beasts.kelog.dto.request.user.UserLoginRequestDTO;
 import com.hunnit_beasts.kelog.dto.response.user.TokenResponseDTO;
+import com.hunnit_beasts.kelog.dto.response.user.UserCreateResponseDTO;
 import com.hunnit_beasts.kelog.entity.domain.User;
 import com.hunnit_beasts.kelog.jwt.JwtUtil;
 import com.hunnit_beasts.kelog.repository.jpa.UserJpaRepository;
-import com.hunnit_beasts.kelog.service.UserService;
+import com.hunnit_beasts.kelog.service.AuthService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class UserServiceImpl implements UserService {
+@Log4j2
+public class AuthServiceImpl implements AuthService {
 
     private final JwtUtil jwtUtil;
     private final UserJpaRepository userJpaRepository;
     private final BCryptPasswordEncoder encoder;
+    private final ModelMapper mapper;
 
     @Override
     public TokenResponseDTO login(UserLoginRequestDTO dto) {
@@ -30,4 +36,12 @@ public class UserServiceImpl implements UserService {
         else
             return new TokenResponseDTO("");
     }
+
+    @Override
+    public UserCreateResponseDTO signUp(UserCreateRequestDTO dto) {
+        dto.setPassword(encoder.encode(dto.getPassword()));
+        User signUpUser = userJpaRepository.save(dto.UserCreateDTOToEntity());
+        return mapper.map(signUpUser, UserCreateResponseDTO.class);
+    }
+
 }

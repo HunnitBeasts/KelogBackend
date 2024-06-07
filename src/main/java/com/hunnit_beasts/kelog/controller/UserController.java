92a56fd20bf0f6b156299d28ca47d24b0e3d958e
yu-teacher <yusm1231@gmail.com>
2024-06-08 05:39:09 +0900

    feat : 회원가입 기능 임시 제작 및 테스트 시행
    회원가입 기능을 임시로 만들었으나 생각보다 동작을 잘 함 그래도 써도 되지 않을까?

---
+++ src/main/java/com/hunnit_beasts/kelog/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.hunnit_beasts.kelog.controller;
+
+import com.hunnit_beasts.kelog.dto.request.user.UserCreateRequestDTO;
+import com.hunnit_beasts.kelog.dto.response.user.UserCreateResponseDTO;
+import com.hunnit_beasts.kelog.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController {
+
+    private final AuthService authService;
+
+    @PostMapping
+    public ResponseEntity<UserCreateResponseDTO> signUp(@RequestBody UserCreateRequestDTO dto){
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(authService.signUp(dto));
+    }
+}

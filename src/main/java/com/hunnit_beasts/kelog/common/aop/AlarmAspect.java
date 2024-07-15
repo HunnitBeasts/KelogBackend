51a773499d369cc03d2b94d533dac6626cbdf7de
Huh Woosol <dnthf652@naver.com>
2024-07-15 21:01:52 +0900

    feat: 알람 작성중(aop)

---
+++ src/main/java/com/hunnit_beasts/kelog/common/aop/AlarmAspect.java
@@ -0,0 +1,17 @@
+package com.hunnit_beasts.kelog.common.aop;
+
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class AlarmAspect{
+
+    @After("@annotation(com.hunnit_beasts.kelog.common.aop.Alarm)")
+    public void alarm(JoinPoint joinPoint){
+    }
+}
